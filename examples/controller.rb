2cda416b5db8ac66b5cadc2d5ad91bf50cf1d41a
Evan Weaver <evan@cloudbur.st>
2007-07-20 17:35:27 +0000

    update examples slightly

--- examples/controller.rb
+++ examples/controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
-  def index        
-    
+
+  def index            
     params['search'] ||= {}
     @options = HashWithAccessorAccess.new((params['options'] || {}).reverse_merge({
-      'models' => Search::MODELS.keys,
+      'models' => nil,
       'to' => 'now',
       'from' => '1 year ago',
       'page' => params[:page] || 1,
@@ -15,19 +15,14 @@ class SearchController < ApplicationController
     }))
     
     @options['raw_filters']['published_at'] = @options.from..@options.to 
-    @search = Search.new(:sphinx, params['search']['query'], @options.no_pass('from', 'to'))
+    @search = Ultrasphinx::Search.new(:sphinx, params['search']['query'], @options.no_pass('from', 'to'))
         
-    begin
-      unless (Chronic.parse(@options.to) and Chronic.parse(@options.from) rescue nil)
-        @error = "Couldn't understand date range."
-      else
-        @search.excerpt unless @search.query.blank?
-      end
-    rescue Object => e
-      raise unless e.is_a? Ultrasphinx::Exception
-      @error = "Search error "
-      @error += e.inspect + e.backtrace.inspect if Rails.development?
-    end       
+    unless (Chronic.parse(@options.to) and Chronic.parse(@options.from) rescue nil)
+      @error = "Couldn't understand date range."
+    else
+      @search.excerpt unless @search.query.blank?
+    end
+
   end
   
 end
