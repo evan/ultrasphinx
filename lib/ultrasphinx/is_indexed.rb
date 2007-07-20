0dbcfd82c12d1aba25f7e32eb72b92486d018d36
Evan Weaver <evan@cloudbur.st>
2007-07-20 22:18:34 +0000

    support association_sql on concats and includes

--- lib/ultrasphinx/is_indexed.rb
+++ lib/ultrasphinx/is_indexed.rb
@@ -9,7 +9,7 @@ module ActiveRecord
       opts.assert_valid_keys [:fields, :concats, :conditions, :includes, :nulls]
       
       Array(opts[:concats]).each do |concat|
-        concat.assert_valid_keys [:model, :conditions, :field, :as, :fields, :association_name]
+        concat.assert_valid_keys [:model, :conditions, :field, :as, :fields, :association_name, :association_sql]
         raise Ultrasphinx::ConfigurationError, "You can't mix regular concat and group concats" if concat[:fields] and (concat[:field] or concat[:model] or concat[:association_name])
         raise Ultrasphinx::ConfigurationError, "Group concats must not have multiple fields" if concat[:field].is_a? Array
         raise Ultrasphinx::ConfigurationError, "Regular concats should have multiple fields" if concat[:fields] and !concat[:fields].is_a?(Array)
--- lib/ultrasphinx/ultrasphinx.rb
+++ lib/ultrasphinx/ultrasphinx.rb
@@ -87,7 +87,7 @@ type = pgsql
         if options_for("source", CONF_PATH)['sql_db'] != ActiveRecord::Base.connection.instance_variable_get("@config")[:database]
            say "warning; configured database name is out-of-date"
            say "please run 'rake ultrasphinx:configure'"
-        end
+        end rescue nil
       end
     end
          
@@ -147,23 +147,35 @@ type = pgsql
           options[:includes].to_a.each do |join|
             join_klass = join[:model].constantize
             association = klass.reflect_on_association(join[:model].underscore.to_sym)
-            join_strings << "LEFT OUTER JOIN #{join_klass.table_name} ON " + 
-              if (macro = association.macro) == :belongs_to 
-                "#{join_klass.table_name}.#{join_klass.primary_key} = #{table}.#{association.primary_key_name}" 
-              elsif macro == :has_one
-                "#{table}.#{klass.primary_key} = #{join_klass.table_name}.#{association.instance_variable_get('@foreign_key_name')}" 
+            if not association 
+              if not join[:association_sql]
+                raise ConfigurationError, "Unknown association from #{klass} to #{join[:model]}"
               else
-                raise ConfigurationError, "Unidentified association macro #{macro.inspect}"
+                join_strings << join[:association_sql]
               end
+            else
+              join_strings << "LEFT OUTER JOIN #{join_klass.table_name} ON " + 
+                if (macro = association.macro) == :belongs_to 
+                  "#{join_klass.table_name}.#{join_klass.primary_key} = #{table}.#{association.primary_key_name}" 
+                elsif macro == :has_one
+                  "#{table}.#{klass.primary_key} = #{join_klass.table_name}.#{association.instance_variable_get('@foreign_key_name')}" 
+                else
+                  raise ConfigurationError, "Unidentified association macro #{macro.inspect}"
+                end
+            end
             column_strings << "#{join_klass.table_name}.#{join[:field]} AS #{join[:as] or join[:field]}"
             remaining_columns.delete(join[:as] || join[:field])
           end
           
           options[:concats].to_a.select{|concat| concat[:model] and concat[:field]}.each do |group|
-            # only has_many's right now
+            # only has_many's or explicit sql right now
             join_klass = group[:model].constantize
-            association = klass.reflect_on_association(group[:association_name] ? group[:association_name].to_sym :  group[:model].underscore.pluralize.to_sym)
-            join_strings << "LEFT OUTER JOIN #{join_klass.table_name} ON #{table}.#{klass.primary_key} = #{join_klass.table_name}.#{association.primary_key_name}" + (" AND (#{group[:conditions]})" if group[:conditions]).to_s # XXX make sure foreign key is right for polymorphic relationships
+            if group[:association_sql]
+              join_strings << group[:association_sql]
+            else
+              association = klass.reflect_on_association(group[:association_name] ? group[:association_name].to_sym :  group[:model].underscore.pluralize.to_sym)
+              join_strings << "LEFT OUTER JOIN #{join_klass.table_name} ON #{table}.#{klass.primary_key} = #{join_klass.table_name}.#{association.primary_key_name}" + (" AND (#{group[:conditions]})" if group[:conditions]).to_s # XXX make sure foreign key is right for polymorphic relationships
+            end
             column_strings << Fields.instance.cast("GROUP_CONCAT(#{join_klass.table_name}.#{group[:field]} SEPARATOR ' ')", group[:as])
             remaining_columns.delete(group[:as])
           end
