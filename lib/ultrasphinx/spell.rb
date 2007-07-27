294ad8bda28079a3bc8f7171487050e35b4ac8d3
Evan Weaver <evan@cloudbur.st>
2007-07-27 05:08:24 +0000

    namespace spell

--- lib/ultrasphinx/spell.rb
+++ lib/ultrasphinx/spell.rb
@@ -1,22 +1,23 @@
 
 # http://blog.evanweaver.com/articles/2007/03/10/add-gud-spelning-to-ur-railz-app-or-wharever
 
-module Spell
-  unloadable rescue nil
+module Ultrasphinx
+  module Spell
   
-  # make sure you've put app.multi (from ../examples) in your system's aspell dictionary folder
-  # you also must run rake ultrasphinx:spelling:build to construct the custom dictionary
-  SP = Aspell.new("app") 
-
-  SP.suggestion_mode = Aspell::NORMAL
-  SP.set_option("ignore-case", "true")
+    # make sure you've put app.multi (from ../examples) in your system's aspell dictionary folder
+    # you also must run rake ultrasphinx:spelling:build to construct the custom dictionary
+    SP = Aspell.new("app") 
   
-  def self.correct string
-    string.gsub(/[\w\']+/) do |word| 
-      unless SP.check(word)
-        SP.suggest(word).first
-      else
-        word
+    SP.suggestion_mode = Aspell::NORMAL
+    SP.set_option("ignore-case", "true")
+    
+    def self.correct string
+      string.gsub(/[\w\']+/) do |word| 
+        unless SP.check(word)
+          SP.suggest(word).first
+        else
+          word
+        end
       end
     end
   end
