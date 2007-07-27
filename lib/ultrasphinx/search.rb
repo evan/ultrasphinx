06b938c7e44bca38e8e268c56f2ca5312192ade0
Evan Weaver <evan@cloudbur.st>
2007-07-27 07:43:53 +0000

    some tests would have helped that refactoring effort

--- lib/ultrasphinx/search.rb
+++ lib/ultrasphinx/search.rb
@@ -5,7 +5,7 @@ module Ultrasphinx
   class Search
     unloadable if RAILS_ENV == "development"
   
-    OPTIONS = {:command => {:search => 0, :excerpt => 1},
+    SPHINX_CLIENT_PARAMS = {:command => {:search => 0, :excerpt => 1},
       #   :status => {:ok => 0, :error => 1, :retry => 2},
       :search_mode => {:all => 0, :any => 1, :phrase => 2, :boolean => 3, :extended => 4},
       :sort_mode => {:relevance => 0, :desc => 1, :asc => 2, :time => 3},
@@ -20,14 +20,26 @@ module Ultrasphinx
       :weights => nil,
       :search_mode => :extended,
     :raw_filters => {}}
-  
-  
+    
+    EXCERPT_OPTIONS = {
+      'before_match' => "<strong>", 'after_match' => "</strong>",
+      'chunk_separator' => "...",
+      'limit' => 256,
+      'around' => 3,
+      # results should respond to one in each group of these, in precedence order, for the excerpting to fire
+      'content_methods' => [[:title, :name], [:body, :description, :content], [:metadata]] 
+    }
+      
+    WITH_SUBTOTALS = true
+      
+    MAX_RETRIES = 4
+    
+    RETRY_SLEEP_TIME = 3
+
     VIEW_OPTIONS = { # XXX this is crappy
       :search_mode => {"all words" => "all", "some words" => "any", "exact phrase" => "phrase", "boolean" => "boolean", "extended" => "extended"}.sort,
     :sort_mode => [["newest first", "desc"], ["oldest first", "asc"], ["relevance", "relevance"]]
     } #, "Time" => :time }
-    
-    MAX_RETRIES = 4
   
     MODELS = begin
       Hash[*open(CONF_PATH).readlines.select{|s| s =~ /^(source \w|sql_query )/}.in_groups_of(2).map{|model, _id| [model[/source ([\w\d_-]*)/, 1].classify, _id[/(\d*) AS class_id/, 1].to_i]}.flatten] # XXX blargh
@@ -53,9 +65,12 @@ module Ultrasphinx
       self.new(*args).run
     end
   
-    def initialize style, query, opts={}
+    def initialize style, query, opts = {}
+      
       opts = {} unless opts
+      
       raise Sphinx::SphinxArgumentError, "Invalid query type: #{style.inspect}" unless QUERY_TYPES.include? style
+      
       @query = (query || "")
       @parsed_query = style == :google ? parse_google(@query) : @query
   
@@ -63,88 +78,46 @@ module Ultrasphinx
       @subtotals = {}
       @response = {}
   
+      # do some typecasting
       @options = DEFAULTS.merge(Hash[*opts.map do |key, value|
         [key.to_sym,
           if value.respond_to?(:to_i) && value.to_i.to_s == value
             value.to_i
           elsif value == ""
             nil
-          elsif value.is_a? String and key.to_s != "sort_by"
+          elsif value.is_a? String
             value.to_sym
           else
             value
           end]
         end._flatten_once])
-        @options[:models] = Array(@options[:models])
+        
+      @options[:models] = Array(@options[:models])
   
-        raise Sphinx::SphinxArgumentError, "Invalid options: #{@extra * ', '}" if (@extra = (@options.keys - (OPTIONS.merge(DEFAULTS).keys))).size > 0
-      end
+      raise Sphinx::SphinxArgumentError, "Invalid options: #{@extra * ', '}" if (@extra = (@options.keys - (SPHINX_CLIENT_PARAMS.merge(DEFAULTS).keys))).size > 0
+      
+    end
   
-      def run(instantiate = true)
-        # set all the options
-        @request = Sphinx::Client.new
-        @request.SetServer(PLUGIN_SETTINGS['server_host'], PLUGIN_SETTINGS['server_port'])
-        offset, limit = options[:per_page] * (options[:page] - 1), options[:per_page]
-        @request.SetLimits offset, limit, [offset + limit, MAX_MATCHES].min
-        @request.SetMatchMode map_option(:search_mode)
-        @request.SetSortMode map_option(:sort_mode), options[:sort_by]      
-  
-        if weights = options[:weights]
-          @request.SetWeights(Fields.instance.select{|n,t| t == 'text'}.map(&:first).sort.inject([]) do |array, field|
-            array << (weights[field] || 1.0)
-          end)
-        end
-  
-        #@request.SetIdRange # never useful
-  
-        unless options[:models].compact.empty?
-          @request.SetFilter 'class_id', options[:models].map{|m| MODELS[m.to_s]}
-        end        
-  
-        options[:raw_filters].each do |field, value|
-          begin
-            unless value.is_a? Range
-              @request.SetFilter field, Array(value)
-            else
-              min, max = [value.first, value.last].map do |x|
-                x._to_numeric if x.is_a? String
-              end
-              unless min.class != max.class
-                min, max = max, min if min > max
-                @request.SetFilterRange field, min, max
-              end
-            end
-          rescue NoMethodError => e
-            raise Sphinx::SphinxArgumentError, "filter: #{field.inspect}:#{value.inspect} is invalid"
-          end
-        end
-        # @request.SetGroup # not useful
-  
-        tries = 0
-        logger.info "** ultrasphinx: searching for #{query.inspect} (parsed as #{@parsed_query.inspect}), options #{@options.inspect}"
-        begin
-          # run the search
-          @response = @request.Query(@parsed_query)
-          logger.info "** ultrasphinx: search returned, error #{@request.GetLastError.inspect}, warning #{@request.GetLastWarning.inspect}, returned #{total}/#{response['total_found']} in #{time} seconds."
-  
-          # get all the subtotals, XXX should be configurable
-          # andrew says there's a better way to do this
-          filtered_request = @request.dup
-          MODELS.each do |key, value|
-            filtered_request.instance_eval { @filters.delete_if {|f| f['attr'] == 'class_id'} }
-            filtered_request.SetFilter 'class_id', [value]
-            @subtotals[key] = @request.Query(@parsed_query)['total_found']
-  #          logger.debug "** ultrasphinx: found #{subtotals[key]} records for sub-query #{key} (filters: #{filtered_request.instance_variable_get('@filters').inspect})"
-          end
-  
-          @results = instantiate ? reify_results(response['matches']) : response['matches']
+    def run(reify = true)
+    
+      @request = build_request_with_options
+      tries = 0
+
+      logger.info "** ultrasphinx: searching for #{query.inspect} (parsed as #{@parsed_query.inspect}), options #{@options.inspect}"
+
+      begin
+        # run the search
+        @response = @request.Query(@parsed_query)
+        logger.info "** ultrasphinx: search returned, error #{@request.GetLastError.inspect}, warning #{@request.GetLastWarning.inspect}, returned #{total}/#{response['total_found']} in #{time} seconds."  
+
+        @subtotals = get_subtotals(@request, @parsed_query) if WITH_SUBTOTALS  
+        @results = response['matches']
+        @results = reify_results(@results) if reify
+                  
       rescue Sphinx::SphinxResponseError, Sphinx::SphinxTemporaryError, Errno::EPIPE => e
         if (tries += 1) <= MAX_RETRIES
           logger.warn "** ultrasphinx: restarting query (#{tries} attempts already) (#{e})"
-          if tries == MAX_RETRIES
-            logger.warn "** ultrasphinx: sleeping..."
-            sleep(3) 
-          end
+          sleep(RETRY_SLEEP_TIME) if tries == MAX_RETRIES
           retry
         else
           logger.warn "** ultrasphinx: query failed"
@@ -156,14 +129,10 @@ module Ultrasphinx
     def excerpt
       run unless run?
       return if results.empty?
-  
-      # results should respond to one in each group of these, in precedence order, for the excerpting to fire
-      # XXX should be configurable
-      possible_methods = [[:title, :name], [:body, :description, :content], [:metadata]] 
-  
+    
       # XXX 'maps' needs to be refactored from a magic array to something that makes sense
       maps = results.map do |record|
-        [record] << possible_methods.map do |methods|
+        [record] << EXCERPT_OPTIONS['content_methods'].map do |methods|
           methods.detect do |x| 
             record.respond_to? x
           end
@@ -184,15 +153,12 @@ module Ultrasphinx
         texts, 
         "complete", 
         @parsed_query.gsub(/AND|OR|NOT|\@\w+/, ""), # XXX hack for query commands, since sphinx doesn't parse them on excerpt
-        'before_match' => "<strong>", 'after_match' => "</strong>",
-        'chunk_separator' => "...",
-        'limit' => 256, # XXX should be configurable
-        'around' => 1).in_groups_of(possible_methods.size)
+        EXCERPT_OPTIONS.except('content_methods')).in_groups_of(EXCERPT_OPTIONS['content_methods'].size)
       
       maps.each_with_index do |record_and_methods, i|
         # override the individual model accessors with the excerpted data
         record, methods = record_and_methods
-        possible_methods.size.times do |m|          
+        EXCERPT_OPTIONS['content_methods'].size.times do |m|          
           record._metaclass.send(:define_method, methods[m]) { responses[i][m] } if methods[m]
         end
       end
@@ -230,6 +196,66 @@ module Ultrasphinx
     end
   
     private
+    
+    def build_request_with_options
+      # XXX ugly, partially stateful build method
+
+      request = Sphinx::Client.new
+
+      request.SetServer(PLUGIN_SETTINGS['server_host'], PLUGIN_SETTINGS['server_port'])
+      offset, limit = options[:per_page] * (options[:page] - 1), options[:per_page]
+      request.SetLimits offset, limit, [offset + limit, MAX_MATCHES].min
+      request.SetMatchMode SPHINX_CLIENT_PARAMS[:search_mode][options[:search_mode]]
+      request.SetSortMode SPHINX_CLIENT_PARAMS[:sort_mode][options[:sort_mode]], options[:sort_by].to_s
+
+      if weights = options[:weights]
+        # XXX we shouldn't really have to hit Fields.instance from within Ultrasphinx::Search
+        request.SetWeights(Fields.instance.select{|n,t| t == 'text'}.map(&:first).sort.inject([]) do |array, field|
+          array << (weights[field] || 1.0)
+        end)
+      end
+
+      unless options[:models].compact.empty?
+        request.SetFilter 'class_id', options[:models].map{|m| MODELS[m.to_s]}
+      end        
+
+      # extract ranged raw filters 
+      options[:raw_filters].each do |field, value|
+        begin
+          unless value.is_a? Range
+            request.SetFilter field, Array(value)
+          else
+            min, max = [value.first, value.last].map do |x|
+              x._to_numeric if x.is_a? String
+            end
+            unless min.class != max.class
+              min, max = max, min if min > max
+              request.SetFilterRange field, min, max
+            end
+          end
+        rescue NoMethodError => e
+          raise Sphinx::SphinxArgumentError, "filter: #{field.inspect}:#{value.inspect} is invalid"
+        end
+      end
+      
+      # request.SetIdRange # never useful
+      # request.SetGroup # never useful
+      
+      request
+    end
+  
+    def get_subtotals(request, query)
+      # XXX andrew says there's a better way to do this
+      subtotals, filtered_request = {}, request.dup
+      
+      MODELS.each do |name, class_id|
+        filtered_request.instance_eval { @filters.delete_if {|f| f['attr'] == 'class_id'} }
+        filtered_request.SetFilter 'class_id', [class_id]
+        subtotals[name] = request.Query(query)['total_found']
+      end
+      
+      subtotals
+    end
   
     def parse_google query
       return unless query
@@ -308,12 +334,7 @@ module Ultrasphinx
         r._metaclass.send(:define_method, "result_index") { i }
       end
       
-    end
-  
-    def map_option opt
-      opt = opt.to_sym
-      OPTIONS[opt][options[opt]] or raise Sphinx::SphinxArgumentError, "Invalid option value :#{opt} => #{options[opt]}"
-    end
+    end  
   
     def logger
       RAILS_DEFAULT_LOGGER
