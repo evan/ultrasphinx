8f8426d91a3e3f4597d2e2e2890570edb4c61105
Evan Weaver <evan@cloudbur.st>
2007-08-14 01:34:20 +0000

    test framework

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+
+$LOAD_PATH << "#{File.dirname(__FILE__)}/../lib"
+RAILS_ROOT = File.dirname(__FILE__)
+RAILS_ENV = "test"
+
+require 'rubygems'
+require 'initializer'
+require 'active_support'
+require 'test/unit'
+require 'ultrasphinx'
+
---
+++ test/unit/parser_test.rb
@@ -0,0 +1,9 @@
+
+require "#{File.dirname(__FILE__)}/../test_helper.rb"
+
+class ParserTest < Test::Unit::TestCase
+
+
+
+end
+
