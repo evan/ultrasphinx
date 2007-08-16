d2e27a6d2f45163b355b0884bbdc243b59b64bc0
Evan Weaver <evan@cloudbur.st>
2007-08-16 23:29:37 +0000

    hey a test

--- test/unit/search_test.rb
+++ test/unit/search_test.rb
@@ -3,7 +3,13 @@ require "#{File.dirname(__FILE__)}/../test_helper.rb"
 
 context "search object" do
 
-  
+  S = Ultrasphinx::Search
+
+  it "rejects invalid keys" do
+    should.raise(Sphinx::SphinxArgumentError) do
+      S.new("query", :wrong => 1)
+    end    
+  end
 
 end
 
