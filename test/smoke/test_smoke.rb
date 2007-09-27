283f75c2b31a4e827189b8cd88f7b30b768634a1
Evan Weaver <evan@cloudbur.st>
2007-09-27 07:08:43 +0000

    got so confused there

--- test/smoke/test_smoke.rb
+++ test/smoke/test_smoke.rb
@@ -28,11 +28,9 @@ class SmokeTest < Test::Unit::TestCase
   end
   
   def test_sort_by_date
-    @q = S.new(:class_names => 'Seller', :sort_by => 'created_at', :sort_mode => 'ascending').run
-    assert_equal((@q.results.map(&:created_at).sort), @q.results.map(&:created_at))
-
-    @q = S.new(:class_names => 'Seller', :sort_by => 'created_at', :sort_mode => 'descending').run
-    assert_not_equal((@q.results.map(&:created_at).sort), @q.results.map(&:created_at))
+    @dates = S.new(:class_names => 'Seller', :sort_by => 'created_at', :sort_mode => 'descending').run.results.map(&:created_at)
+    assert_equal @dates.sort.reverse, @dates
+    assert_not_equal @dates.sort, @dates
   end
  
 
