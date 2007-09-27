e805b231ca09e3bc491366d303b7122f56248067
Evan Weaver <evan@cloudbur.st>
2007-09-27 08:46:41 +0000

    done

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,7 +1,7 @@
 
 Dir.chdir(File.dirname(__FILE__)) do
   Dir["unit/*.rb", "integration/*.rb"].each do |file|
-    puts "*** #{file} ***"
+    puts "\n*** #{file} ***"
     system("ruby #{file}")
   end
 end
