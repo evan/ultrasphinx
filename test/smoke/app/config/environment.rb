8c7a355f48b5468b1498116b78f5849e857fe60c
Evan Weaver <evan@cloudbur.st>
2007-09-27 02:59:11 +0000

    why didnt respond to work

--- test/smoke/app/config/environment.rb
+++ test/smoke/app/config/environment.rb
@@ -2,7 +2,5 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  if config.action_controller.respond_to? :"session="  
     config.action_controller.session = { :session_key => "_myapp_session", :secret => "this is a super secret phrase" }
-  end
 end
