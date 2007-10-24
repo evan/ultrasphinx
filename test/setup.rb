2700905bd2cfab528f7531d6d6faf6f172f5269c
Evan Weaver <evan@cloudbur.st>
2007-10-24 10:17:39 +0000

    rm symlink

--- test/setup.rb
+++ test/setup.rb
@@ -5,6 +5,9 @@ puts ["Please make sure that test/integration/app/vendor/rails is symlinked",
   "to a Rails trunk checkout in order for the rake tasks to run properly."]
 
 Dir.chdir "#{File.dirname(__FILE__)}/integration/app/" do
+  Dir.chdir "vendor/plugins" do
+    system("ln -s ../../../../../ ultrasphinx")
+  end
   system("rake db:create")
   system("rake db:migrate db:fixtures:load")
   system("rake us:boot")
