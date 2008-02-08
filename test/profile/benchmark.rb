c089379eaed9c8f97e060f901355df8b3e5e24c2
Evan Weaver <evan@cloudbur.st>
2008-02-08 09:18:49 +0000

    Fuller benchmark.
    git-svn-id: http://fauna.rubyforge.org/svn/ultrasphinx/branches/multiget@1654 c1ad287d-65d5-445d-b84c-e64f8492f1e6

--- test/profile/benchmark.rb
+++ test/profile/benchmark.rb
@@ -4,19 +4,41 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 require 'benchmark'
 require 'ruby-prof'
 
-prof = ENV['PROF']
+#def debugger; end
+# require 'ruby-debug'
 
-S = Ultrasphinx::Search
-E = Ultrasphinx::UsageError
+PROF = ENV['PROF']
+TIMES = 50
 
-RubyProf.start if prof
+$options = [{:query => 'seller'}]
+
+#$search = Ultrasphinx::Search.new(*$options).run
+#$sellers = Seller.find_all_by_id($search.results.select{|o| o.is_a? Seller}.map(&:id))
+#$users = User.find_all_by_id($search.results.select{|o| o.is_a? User}.map(&:id))
+#
+## Mocha didn't work; don't know why
+#class Riddle::Client
+#  def query(*args); $search.response; end
+#end
+#class Seller
+#  def self.find_all_by_id(*args); $sellers; end
+#end
+#class User
+#  def self.find_all_by_id(*args); $users; end
+#end
 
 Benchmark.bm(20) do |x|
   x.report("simple") do 
-    100.times do
-      @s = S.new(:query => 'seller').run
+    TIMES.times do
+       Ultrasphinx::Search.new(*$options).run
     end
+  end
+  x.report("excerpt") do 
+    RubyProf.start if PROF
+    TIMES.times do
+       Ultrasphinx::Search.new(*$options).excerpt
+    end
+    RubyProf::GraphPrinter.new(RubyProf.stop).print(STDOUT, 0) if PROF  
   end
 end
 
-RubyProf::GraphPrinter.new(RubyProf.stop).print(STDOUT, 0) if prof  
